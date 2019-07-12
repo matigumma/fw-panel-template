f1c789bfb0e458965125bcb303b02351c53aed2c
matigumma <matiasgumma@gmail.com>
2019-07-12 19:17:11 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import './styles/styles.scss';
 import './static/img/banana.jpg';
 
 const $ = require('jquery');
+import 'bootstrap';
 
 $(document).ready(()=>console.log('$ ready from app.js'));
 $('form').submit((e)=>e.preventDefault());
